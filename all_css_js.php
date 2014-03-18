6e599650d2a1a350083946e9d78e7d2415958a0e
ThaiMa <maquocthai@gmail.com>
2014-03-18 16:28:33 +0700

    HTML basic
    template FE

---
+++ all_css_js.php
@@ -0,0 +1,23 @@
+<!-- Basic Page Needs
+  ================================================== -->
+<meta charset="utf-8">
+<link rel="shortcut icon" href="images/favicon.ico" />
+<title>STK Slimmerindezon</title>
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- CSS
+  ================================================== -->
+<link rel="stylesheet" href="css/reset.css">
+<link rel="stylesheet" href="css/skeleton.css">
+<link rel="stylesheet" href="css/font.css">
+<link rel="stylesheet" href="css/style.css">
+
+<script src="js/jquery-1.9.1.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+
+<script src="js/class.SiteMain.js"></script>
+
+<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+
---
+++ js/class.SiteMain.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+var SiteMain = (function() {
+	function init(){
+		
+	}
+	
+	return {
+		init:init
+	}
+})();		
+
+$(document).ready( function() {
+	SiteMain.init();
+});
+
